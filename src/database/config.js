ffd45430b65533cf53c06b7864bd5e092ed20273
joseilagosb <joseilagosb@gmail.com>
2024-04-25 17:07:03 -0400

    agregado dockerfile, modificados nombres env variables

--- src/database/config.js
+++ src/database/config.js
@@ -3,10 +3,10 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
     port: "5432",
     dialect: "postgres",
     // logging: false,
